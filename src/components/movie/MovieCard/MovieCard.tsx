768908f3f973192f0ab017d5f6fe3cd598a283e2
Reload0213 <dldudwn7227@gmail.com>
2025-03-03 17:53:01 +0900

    Fix: 북마크 추가

--- src/components/movie/MovieCard/MovieCard.tsx
+++ src/components/movie/MovieCard/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { Movie } from "@/types/movie";
 
+import MovieBookmark from "./MovieBookmark";
 import MovieDate from "./MovieDate";
 import MoviePoster from "./MoviePoster";
 import MovieRating from "./MovieRating";
@@ -17,8 +18,11 @@ const MovieCard = ({ movie }: MovieCardProps) => {
 				src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
 				alt={movie.title}
 			/>
-			<MovieTitle ko_title={movie.korean_title} en_title={movie.title} />
-			<MovieRating value={movie.vote_average} />
+			<div className="relative flex-1 flex flex-col">
+				<MovieTitle ko_title={movie.korean_title} en_title={movie.title} />
+				<MovieRating value={movie.vote_average} size='lg' />
+				<MovieBookmark movieId={movie.id} isBookmarked={movie.is_bookmarked} />
+			</div>
 			<MovieDate date={movie.release_date} />
 		</div>
 	);
